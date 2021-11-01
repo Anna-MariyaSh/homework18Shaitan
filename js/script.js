64277e4e5886d4ea6b1e26c2fbbad773940e690c
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-11-01 22:22:38 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,82 @@
+   //task 1
+// arr1 = [1, 2, 3];
+// arr2 = [4, 5, 6];
+// arr3 = arr1.concat(arr2);
+// console.log(arr3);
+ //task 2
+ // arr1 = [1, 2, 3];
+ // arr1.reverse();
+ // console.log(arr1);
+//task 3
+// arr1 = [1, 2, 3];
+// arr1.push(4, 5, 6);
+// console.log(arr1);
+//task 4
+// arr1 = [1, 2, 3];
+// arr1.unshift(4, 5, 6);
+// console.log(arr1);
+//task 5
+// arr = ['js', 'css', 'jq'];
+// alert(arr[0]);
+//task 6
+// arr = ['js', 'css', 'jq'];
+// alert(arr[arr.length-1]);
+//task 7
+//  arr1 = [1, 2, 3, 4, 5];
+//  arr2 = arr1.slice(0, 3);
+// console.log(arr2);
+//task 8
+// arr1 = [1, 2, 3, 4, 5];
+// arr2 = arr1.slice(3, 5);
+// console.log(arr2);
+//task 9
+// arr1 = [1, 2, 3, 4, 5];
+// arr1.splice(1, 2);
+// console.log(arr1);
+//task 10
+// arr1 = [1, 2, 3, 4, 5];
+// arr2 = arr1.slice(1, 4);
+// console.log(arr2);
+//task 11
+// arr1 = [1, 2, 3, 4, 5];
+// arr1.splice(3, 0, "a", "d", "c");
+// console.log(arr1);
+// task 12
+// arr1 = [1, 2, 3, 4, 5];
+// arr1.splice(1, 0, "a", "b");
+// arr1.splice(6, 0, "c");
+// arr1.splice(8, 0, "e");
+// console.log(arr1);
+// task 13
+// arr1 = [3, 4, 1, 2, 7];
+// arr1.sort();
+// console.log(arr1);
+// task 14
+// arr1 = [5, 6, 7, 8, 9];
+// const reducer = (previousValue, currentValue) => previousValue + currentValue;
+// let result = arr1.reduce(reducer);
+// console.log(result);
+//task 15
+// arr1 = [5, 6, 7, 8, 9];
+// arr2 = arr1.map(
+//   function(qwerty) {
+//     return qwerty * qwerty;
+//   }0
+// );
+// console.log(arr2);
+//task 16
+// arr = [1,-3, 5, 6,-7, 8, 9,-11];
+// console.log(arr.filter((item) => item < 0));
+//task 17
+// arr = [1,-3, 5, 6,-7, 8, 9,-11];
+// console.log(arr.filter((item) => item % 2 === 0));
+//task 18
+// arr = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
+// console.log(arr.filter((item) => item.length > 5));
+//task 19
+// arr = [1, 2, [3, 4], 5, [6, 7]];
+// console.log(arr.filter((item) => Array.isArray(item)));
+//task 20
+// arr = [5,-3, 6,-5, 0,-7, 8, 9];
+// arr1 = arr.filter((item) => item < 0);
+// console.log(arr1.length);
